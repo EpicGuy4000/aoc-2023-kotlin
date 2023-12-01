e0c9b75c0760debe457321cf187e190c1ef109b0
EpicGuy4000 <enjakovic@gmail.com>
2023-12-01 11:44:30 +0100

    Day one

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,45 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            val first = it.first { c -> c.isDigit() }.digitToInt()
+            val last = it.last { c -> c.isDigit() }.digitToInt()
+            first * 10 + last
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val stringsAndValues = mutableMapOf(
+            Pair("one", "1"),
+            Pair("two", "2"),
+            Pair("three", "3"),
+            Pair("four", "4"),
+            Pair("five", "5"),
+            Pair("six", "6"),
+            Pair("seven", "7"),
+            Pair("eight", "8"),
+            Pair("nine", "9")
+        )
+
+        return input.sumOf {
+            var newIt = it
+            stringsAndValues.onEach { str -> newIt = newIt.replace(str.key, str.key + str.value + str.key) }
+
+            val first = newIt.first { c -> c.isDigit() }.digitToInt()
+            val last = newIt.last { c -> c.isDigit() }.digitToInt()
+
+            first * 10 + last
+        }
     }
 
     // test if implementation meets criteria from the description, like:
+    checkExpectedValue(part2(listOf("two1nine")), 29)
+    checkExpectedValue(part2(listOf("eightwothree")), 83)
+
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    checkExpectedValue(part1(testInput), 142)
+    val testInput2 = readInput("Day01_test2")
+    checkExpectedValue(part2(testInput2), 281)
+
 
     val input = readInput("Day01")
     part1(input).println()
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,8 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun <T> checkExpectedValue(actual: T, expected: T) {
+    println("expected $expected, actual $actual")
+    check(expected == actual)
+}
